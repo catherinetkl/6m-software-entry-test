c3424e0d0866fb225f2c5488cb90a4dfb2842835
Catherine <70590533+catherinetkl@users.noreply.github.com>
2023-07-23 22:37:42 +0800

    feat: Link external.js to q5.js

--- src/q5.js
+++ src/q5.js
@@ -5,7 +5,16 @@
     - To test if the linking works, open q5-output.html in the browser and it should show "Hello World!"
 */
 
+/* Pre-requisites
+    - Install a development server globally on your machine using Node.js:
+    npm install -g http-server
+    - navigate to the directory where q5-output.html, q5.js, and external.js are located
+    - Start development server:
+    http-server
+*/
+
 // Task 1: Add code here
+import print from './external.js';
 
 // Do not change the code below
 document.querySelector("#test").innerHTML = print();
