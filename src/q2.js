402f2e01b71013c57838b83c4be567f845a25552
Catherine <70590533+catherinetkl@users.noreply.github.com>
2023-07-23 21:26:41 +0800

    Create patientNames array, add patients using push, and implement listPatients function

--- src/q2.js
+++ src/q2.js
@@ -10,17 +10,25 @@
 */
 
 
-// Task 1: Add code here
+// Task 1: Declare an array that is going to be used to store patient's name
+const patientNames = [];
 
-function addPatient(patientName){
-    // Task 2: Add code here
+// Task 2: Add code to add patient's name into the array declared in task 1
+function addPatient(patientName) {
+  patientNames.push(patientName);
 }
 
-// Task 3: Add code here
+/* Task 3: Implement listPatient() function to print all patient's name
+stored in the array */
+function listPatient() {
+  console.log(patientNames);
+}
 
-
+// Add patients
 addPatient("John");
 addPatient("Mary");
 addPatient("Mark");
 
-listPatient(); // This should list ["John", "Mary", "Mark"]
+/* invoke the listPatient() function to print all patients' names
+stored in the array ["John", "Mary", "Mark"] */
+listPatient();
