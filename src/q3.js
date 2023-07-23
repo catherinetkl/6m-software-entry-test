56a461333af01031ebdc06d8b4ffba40b2cad77d
Catherine <70590533+catherinetkl@users.noreply.github.com>
2023-07-23 21:33:34 +0800

    Add 'breed' property w/ value 'Poodle', print keys & values in for-in loop

--- src/q3.js
+++ src/q3.js
@@ -7,9 +7,14 @@
 */
 
 const dog = {
-    petName:"Bobby"
+  petName: "Bobby"
 }
 
-// Task 1: Add code here
+// Task 1: Add a new property "breed" with the value "Poodle"
+dog.breed = "Poodle";
 
-// Task 2: Add code here
+/* Task 2: Implement a for-in loop to loop through the keys in "dog" object
+and for each key, print both the key and its corresponding value */
+for (let key in dog) {
+  console.log(key + ": " + dog[key]);
+}
